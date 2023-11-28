b54bc60577a1acf037bc440431fb389c92b64b4e
farhanteacha <152068469+farhanteacha@users.noreply.github.com>
2023-11-28 20:36:49 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923451371205', 'Farhan', true],
+  ['923451371205', 'Farhan', true], 
   [''] 
 ] //Number of owners
 
